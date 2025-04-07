31f761faca014183b5cb776ad512bf8c89ff9d61
chodadoo <chodadoo@gmail.com>
2025-04-07 22:47:36 +0900

    1. 최초 커밋

---
+++ GlimWork/GlimWorkDlg.cpp
@@ -0,0 +1,346 @@
+﻿
+// GlimWorkDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "GlimWork.h"
+#include "GlimWorkDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CGlimWorkDlg 대화 상자
+
+
+
+CGlimWorkDlg::CGlimWorkDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_GLIMWORK_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CGlimWorkDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CGlimWorkDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_LBUTTONUP()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_MOUSEMOVE()
+END_MESSAGE_MAP()
+
+
+// CGlimWorkDlg 메시지 처리기
+
+BOOL CGlimWorkDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	CRect rect;
+	GetDlgItem(IDC_STATIC)->GetWindowRect(&rect);
+	ScreenToClient(&rect);
+
+	m_Image.Create(rect.Width(), rect.Height(), 8);
+
+	static RGBQUAD rgb[256];
+	for (int i = 0; i < 256; i++)
+		rgb[i].rgbRed = rgb[i].rgbGreen = rgb[i].rgbBlue = i;
+	m_Image.SetColorTable(0, 256, rgb);
+
+	unsigned char* fm = (unsigned char*)m_Image.GetBits();
+
+	// '(전체 줄 - 1) * 한줄의 바이트수'를 빼서 시작 줄의 주소로 변경한다.
+	fm -= m_Image.GetWidth() * (m_Image.GetHeight() - 1) * (8 / 8);
+
+	memset(fm, 0xFF, m_Image.GetWidth() * m_Image.GetHeight());
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CGlimWorkDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CGlimWorkDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CRect rect;
+		GetDlgItem(IDC_STATIC)->GetWindowRect(&rect);
+		ScreenToClient(&rect);
+
+		CClientDC DC(this);
+		m_Image.Draw(DC, rect);		
+
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CGlimWorkDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CGlimWorkDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	
+	// UI 반응 범주
+	CRect rect;
+	GetDlgItem(IDC_STATIC)->GetWindowRect(&rect);
+	ScreenToClient(&rect);
+
+	if (!rect.PtInRect(point))
+	{
+		return;
+	}
+	
+	// 클릭 오브젝트 찾기
+	for (int i = 0; i < CircleMaxCount; i++)
+	{
+		if (m_Circle[i]._Create && m_Circle[i].PtInRect(point))
+		{
+			m_Circle[i]._Moving = true;
+			return;
+		}
+	}
+
+	// 생성
+	if (CircleMaxCount <= m_dCircleCnt)
+	{
+		return;
+	}
+
+	m_Circle[m_dCircleCnt++].Create(rect, point);
+
+	CirclePrc();
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+// 오브젝트들에게 마우스 다운 노티
+
+void CGlimWorkDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	
+	// UI 반응 범주
+	CRect rect;
+	GetDlgItem(IDC_STATIC)->GetWindowRect(&rect);
+	ScreenToClient(&rect);
+
+	if (!rect.PtInRect(point))
+	{
+		return;
+	}
+
+	// 클릭 오브젝트 찾기
+	for (int i = 0; i < CircleMaxCount; i++)
+	{
+		if (m_Circle[i]._Moving)
+		{
+			m_Circle[i]._Moving = false;
+		}
+	}
+	
+	CDialogEx::OnLButtonUp(nFlags, point);
+}
+
+// 오브젝트들에게 마우스 업 노티
+
+void CGlimWorkDlg::CirclePrc(void)
+{
+	// TODO: 여기에 구현 코드 추가.
+	
+	unsigned char* fm = (unsigned char*)m_Image.GetBits();
+
+	// '(전체 줄 - 1) * 한줄의 바이트수'를 빼서 시작 줄의 주소로 변경한다.
+	fm -= m_Image.GetWidth() * (m_Image.GetHeight() - 1) * (8 / 8);
+	memset(fm, 0xFF, m_Image.GetWidth() * m_Image.GetHeight());
+
+	for (int i = 0; i < m_dCircleCnt; i++)
+	{
+		if (m_Circle[i]._Create)
+			m_Circle[i]._Drwa((unsigned char *)m_Image.GetBits(), m_Image.GetPitch());
+	}
+
+	// 세점의 외접원
+	if (CircleMaxCount <= m_dCircleCnt)
+	{
+		unsigned char* fm2 = (unsigned char*)m_Image.GetBits();
+
+		CPoint _Center = GetCenter(m_Circle[0]._ClickPoint, m_Circle[1]._ClickPoint, m_Circle[2]._ClickPoint);
+
+		// 중심점에서 반지름
+		int distance = sqrt(pow(_Center.x - m_Circle[0]._ClickPoint.x, 2) + pow(_Center.y - m_Circle[0]._ClickPoint.y, 2));
+
+		int nStartX = _Center.x - distance;
+		int nStartY = _Center.y - distance;
+		int nCenterX = _Center.x;
+		int nCenterY = _Center.y;
+
+		for (int i = nStartX; i < nStartX + (distance * 2); i++)
+		{
+			for (int j = nStartY; j < nStartY + (distance * 2); j++)
+			{
+				// 픽셀단위로 원에 해당 여부를 판정
+				if (isInCircle(i, j, nCenterX, nCenterY, distance))
+					fm2[(j * m_Image.GetPitch()) + i] = 0x00;
+			}
+		}
+	}
+
+	OnPaint();
+}
+
+bool CGlimWorkDlg::isInCircle(int i, int j, int nCenterX, int nCenterY, int nRadius)
+{
+	bool bRet = false;
+
+	double dX = i - nCenterX;
+	double dY = j - nCenterY;
+	double dDist = dX * dX + dY * dY;
+
+	if (dDist < nRadius * nRadius) {
+		bRet = true;
+	}
+
+	return bRet;
+}
+
+// 외접원 계산식 'https://m.blog.naver.com/hileejaeho/221751241141'
+CPoint CGlimWorkDlg::GetCenter(CPoint pt1, CPoint pt2, CPoint pt3)
+{
+	double A = pt2.x - pt1.x;
+	double B = pt2.y - pt1.y;
+	double C = pt3.x - pt1.x;
+	double D = pt3.y - pt1.y;
+	double E = A * (pt1.x + pt2.x) + B * (pt1.y + pt2.y);
+	double F = C * (pt1.x + pt3.x) + D * (pt1.y + pt3.y);
+	double G = 2.0 * (A * (pt3.y - pt2.y) - B * (pt3.x - pt2.x));
+	if (G == 0)
+		return pt1;
+
+	CPoint ptCenter;
+	ptCenter.x = (D * E - B * F) / G;
+	ptCenter.y = (A * F - C * E) / G;
+
+	return ptCenter;
+}
+
+void CGlimWorkDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	for (int i = 0; i < CircleMaxCount; i++)
+	{
+		if (m_Circle[i]._Moving)
+		{
+			m_Circle[i].Hold(point);
+		}
+	}
+
+	CirclePrc();
+
+	CDialogEx::OnMouseMove(nFlags, point);
+}
---
+++ GlimWork/GlimWorkDlg.h
@@ -0,0 +1,165 @@
+﻿
+// GlimWorkDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CGlimWorkDlg 대화 상자
+class CGlimWorkDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CGlimWorkDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GLIMWORK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+private:
+	void CirclePrc(void);
+	bool isInCircle(int i, int j, int nCenterX, int nCenterY, int nRadius);
+	CPoint GetCenter(CPoint pt1, CPoint pt2, CPoint pt3);
+	int m_dCircleCnt;
+	CImage m_Image;
+	
+	static const int CircleMaxCount = 3;
+
+	struct _Circle
+	{
+		CPoint _ClickPoint;
+		CRect _CirclePos;
+		int _nRadius;
+		bool _Moving;
+		bool _Create;
+
+		// 계산하기 힘들고 일단 함수를 다 까먹어서 오프셋으로 한다.
+		int _Xoffset;
+		int _Yoffset;
+
+		_Circle()
+		{
+			_nRadius = 20;
+			_Moving = false;
+			_Create = false; // 이게 뭐여 ㅋㅋㅋ
+		}
+
+		void Create(CRect& rect/*offset*/, CPoint point)
+		{
+			_Xoffset = -rect.left;
+			_Yoffset = -rect.top;
+
+			_Create = true;
+
+			point.x += _Xoffset;
+			point.y += _Yoffset;
+
+			_ClickPoint.x = point.x;
+			_ClickPoint.y = point.y;
+
+			_CirclePos.left = point.x - _nRadius;
+			_CirclePos.right = _CirclePos.left + _nRadius;
+			_CirclePos.top = point.y - _nRadius;
+			_CirclePos.bottom = _CirclePos.top + _nRadius;
+		}
+
+		void Hold(CPoint& point)
+		{
+			point.x += _Xoffset;
+			point.y += _Yoffset;
+
+			_ClickPoint.x = point.x;
+			_ClickPoint.y = point.y;
+
+			_CirclePos.left = point.x - _nRadius;
+			_CirclePos.right = _CirclePos.left + _nRadius;
+			_CirclePos.top = point.y - _nRadius;
+			_CirclePos.bottom = _CirclePos.top + _nRadius;
+		}
+
+		bool PtInRect(CPoint point)
+		{
+			point.x += _Xoffset;
+			point.y += _Yoffset;
+
+			CRect rect(_ClickPoint.x - _nRadius, _ClickPoint.y - _nRadius, _ClickPoint.x + _nRadius, _ClickPoint.y + _nRadius);
+
+			return rect.PtInRect(point);
+		}
+
+		void _Drwa(unsigned char *p, int nPitch)
+		{
+			int nStartX = _ClickPoint.x - _nRadius;
+			int nStartY = _ClickPoint.y - _nRadius;
+			int nCenterX = _ClickPoint.x;
+			int nCenterY = _ClickPoint.y;
+
+			for (int i = nStartX; i < nStartX + (_nRadius * 2); i++)
+			{
+				for (int j = nStartY; j < nStartY + (_nRadius * 2); j++)
+				{
+					// 픽셀단위로 원에 해당 여부를 판정
+					if (isInCircle(i, j, nCenterX, nCenterY, _nRadius))
+						p[(j * nPitch) + i] = 0x00;
+				}
+			}
+		}
+
+		bool isInCircle(int i, int j, int nCenterX, int nCenterY, int nRadius)
+		{
+			bool bRet = false;
+
+			double dX = i - nCenterX;
+			double dY = j - nCenterY;
+			double dDist = dX * dX + dY * dY;
+
+			if (dDist < nRadius * nRadius) {
+				bRet = true;
+			}
+
+			return bRet;
+		}
+	};
+
+	// 리스트로 바꿔야 하는데 다 까먹었네, 비교 연산자 오버로딩 펑터 뭐더라;;;
+	_Circle m_Circle[CircleMaxCount];
+	_Circle *ActiveCircle = NULL;
+public:
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+};
+
+// ToDo
+/*
+-아래 그림의 순서대로 조작하여 요구사항들을 모두 만족하는 프로그램을 작성합니다.
+- 세 번째 클릭 이후에 클릭 지점 3개를 모두 지나가는 정원 1개를 그립니다.
+- 클릭 지점 원을 그릴 때의 반지름 크기는 사용자로부터 입력 받습니다.
+- 각 클릭 지점 원의 중심 좌표를 UI에 표시합니다.
+- 네 번째 클릭부터는 클릭 지점 원을 그리지 않습니다.
+- 세 클릭 지점을 지나가는 정원의 내부는 채워지지 않아야 하며, 가장자리 두께는 사용자로부터 입력 받습니다.
+- 클릭 지점 3개 중 하나를 클릭하고 드래그 했을 때 정원을 다시 그립니다.
+- 이 때, 마우스 커서 좌표가 바뀌는 동안 즉, 마우스 드래그 상태가 끝날 때 까지 정원이 계속해서 이동하며 그려져야 합니다.
+- [초기화] 버튼을 누르면 그려졌던 모든 내용들을 삭제하고 처음부터 입력 받을 수 있는 상태가 되어야 합니다.
+- 정원이 그려진 상태에서[랜덤 이동] 버튼을 누르면 3개의 클릭 지점 원 모두를 랜덤한 위치로 이동시킵니다.
+- 이 때, 정원 또한 마찬가지로 다시 그려져야 합니다.
+- 랜덤한 위치로 이동 및 정원 그리기 동작을 초당 2회, 총 10번 자동으로 반복하되 메인UI가 프리징 상태가 되지 않도록 별도 쓰레드로 구현해야 합니다.
+- MFC Dialog 기반 프로젝트로 작성해야 합니다.
+- 클릭 지점 원과 정원을 그릴 때 CDC 클래스를 사용하면 안됩니다. (안내한 유튜브 영상 참고)
+*/
